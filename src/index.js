a36e9513449e840ed818f63ca5f72ad5d42da1a5
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-06-17 14:22:21 -0400

    refactor: split css into multiple files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import "./style.css";
+import "./styles/global.css";
+import "./styles/header.css";
+import "./styles/section.css";
+import "./styles/footer.css";
+import "./styles/queries.css";
 
 
