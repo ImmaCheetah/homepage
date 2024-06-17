ea863f7398b1d256919174b0b45c391535039e9d
Davit <29075792+ImmaCheetah@users.noreply.github.com>
2024-06-17 16:44:47 -0400

    create snapshot for package removal

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@ import "./styles/section.css";
 import "./styles/footer.css";
 import "./styles/queries.css";
 
+import battleship from "./assets/img/battleship.png";
+import calculator from "./assets/img/calculator.png";
+import sketch from "./assets/img/sketch.png";
+import tictactoe from "./assets/img/tictactoe.png";
+import todo from "./assets/img/todo.png";
+import weather from "./assets/img/weather.png";
+import twoCheetahsCropped from "./assets/img/two-cheetahs-cropped.png";
+import twoCheetahs from "./assets/img/two-cheetahs.jpg";
+import cheetahPortrait from "./assets/img/cheetah-portrait.jpg";
+
+
+
 
